72e27c0aa6a2b0f49631763af555b8713d797635
analaura2323 <analauracortessoares53@gmail.com>
2024-10-21 15:08:41 -0300

    Add files via upload

---
+++ java/js.js
@@ -0,0 +1,8 @@
+document.getElementById('alertButton').addEventListener('click', () => {//
+    alert('Obrigado por acessar!');
+  });
+
+  function redirecionar() {
+      window.location.href = "p2.html";  // Redireciona o usuário para outra página
+      //mandar para outro lugar/pagina
+  }
